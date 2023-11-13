45596b69da9786f97d390730d0f104a97f90203e
moahritz <moritzjleithner@gmail.com>
2023-11-13 21:28:37 +0100

    small improvements to OrderBook

--- HW1_MoritzJakob.py
+++ HW1_MoritzJakob.py
@@ -1,28 +1,75 @@
+####### 1. Implementing a limit oder market
+
 class OrderBook(object):
     def __init__(self):
-         self.asks = []
-         self.bids = []
-         #self.tick = 0.1
+        self.asks = []
+        self.bids = []
+        self.min_bid = None
+        self.max_ask = None
+
     def add_order(self, order):
         if order.side == 'sell':
-            self.asks.append(order)
+            if self.min_bid is not None and order.limit_price <= self.min_bid:
+                self.match_orders(order, self.bids)
+            else:
+                self.asks.append(order)
+                if self.max_ask is None or order.limit_price > self.max_ask:
+                    self.max_ask = order.limit_price
         elif order.side == 'buy':
-            self.bids.append(order)
-        # You might want to sort the orders after adding them
-        # LimitOrderBook.asks.sort(key=lambda x: x.limit_price)
-        # LimitOrderBook.bids.sort(key=lambda x: x.limit_price, reverse=True)
+            if self.max_ask is not None and order.limit_price >= self.max_ask:
+                self.match_orders(order, self.asks)
+            else:
+                self.bids.append(order)
+                if self.min_bid is None or order.limit_price < self.min_bid:
+                    self.min_bid = order.limit_price
+
+    def match_orders(self, new_order, orders):
+        for order in orders:
+            if new_order.side == 'buy' and order.limit_price <= new_order.limit_price:
+                trade_price = order.limit_price
+                trade_size = min(new_order.size, order.size)
+                trade = Trade(new_order.ID, order.ID, trade_price, trade_size)
+                print("Trade: %s" % trade)
+                orders.remove(order)
+                new_order.size -= trade_size
+                if new_order.size == 0:
+                    break
+            elif new_order.side == 'sell' and order.limit_price >= new_order.limit_price:
+                trade_price = order.limit_price
+                trade_size = min(new_order.size, order.size)
+                trade = Trade(new_order.ID, order.ID, trade_price, trade_size)
+                print("Trade: %s" % trade)
+                orders.remove(order)
+                new_order.size -= trade_size
+                if new_order.size == 0:
+                    break
+            
+        # Update min_bid and max_ask
+        self.min_bid = min([bid.limit_price for bid in self.bids]) if self.bids else None
+        self.max_ask = max([ask.limit_price for ask in self.asks]) if self.asks else None
 
     def display(self):
-         print("Bids: ")
-         for bid in self.bids:
-           print(bid)
-         print("Asks: ")
-         for ask in self.asks:
-              print(ask)
+        print("Total number of orders: %s" % Order.number)
+        print("Min bid: %s" % self.min_bid)
+        print("Max ask: %s" % self.max_ask)
+        print("Bids: ")
+        for bid in self.bids:
+            print(bid)
+        print("Asks: ")
+        for ask in self.asks:
+            print(ask)
         
 order_book = OrderBook()
 
+class Trade(object):
+    def __init__(self, buy_order_id, sell_order_id, price, size):
+        self.buy_order_id = buy_order_id
+        self.sell_order_id = sell_order_id
+        self.price = price
+        self.size = size
 
+    def __str__(self):
+        return "Buy Order ID: %s, Sell Order ID: %s, Price: %s, Size: %s" % (self.buy_order_id, self.sell_order_id, self.price, self.size)
 
 
 
@@ -42,9 +89,7 @@ class Order(object):
 
     def get_number(self):
         return self.ID
-    def __del__(self):
-         Order.number -= 1
-         print("Order (ID: %s) deleted" %(self.ID))    
+
 
 class Ask(Order):
 
@@ -70,38 +115,32 @@ class Bid(Order):
 
 ######### TEST AREA ########
 order1 = Ask(100.5)
+order_book.display()
 order2 = Ask(103)
-print(order2)
-
+order_book.display()
+#print(order2)
 
 order3 = Bid(102.5)
-order4 = Bid(101)
-print(order3)
-
-Order.number
-
-print(order1)
-
-order5 = Ask(34)
-print(order5)
-Order.number
-del order5
-Order.number
-del order3
-Order.number
-print(order4)
-
-
-ASKS = [order1, order2, order5]
-ASKS.sort(key=lambda x: x.limit_price)
-ASKS[0].limit_price
-
-BIDS = [order3, order4]
-BIDS.sort(key=lambda x: x.limit_price, reverse=True)
-BIDS[0].limit_price
-
-
-######################### NEW STUFF ############################
-
 
 order_book.display()
+order4 = Bid(101)
+order_book.display()
+#print(order3)
+
+Order.number
+
+#print(order1)
+
+order5 = Ask(34)
+#print(order5)
+order_book.display()
+order6 = Bid(104)
+order_book.display()
+order7 = Ask(100)
+order_book.display()
+order8 = Bid(105)
+order_book.display()
+######################### NEW STUFF ############################
+
+##### 2. Generating random numbers and simulating order flow
+
