db7e63a683ffd45c209d7d9c6f2f499d4bec9045
moahritz <moritzjleithner@gmail.com>
2023-11-30 12:51:36 +0100

    getting lost

--- Auxiliary Stuff/snippets.py
+++ Auxiliary Stuff/snippets.py
@@ -38,14 +38,65 @@ def mean_monte_carlo_simulation(S0, sigma, T, dt, r, n_simulations):
 
 
 
+
+
+
+
+
+
+S0 = 100  # Initial stock price
 dt = 1 / 252  # Time step
 T = 1  # Time horizon
-S0 = 100  # Initial stock price
-
-
 v = 0.2  # Volatility
 r = 0.05  # Risk-free interest rate
+K = 100  # Strike price
 
-W_T = np.random.standard_normal(int(T/dt))
-S_T = S0 * np.exp((r - 0.5 * v ** 2) * T + v * np.sqrt(T) * W_T)
-mcA = np.exp(-r * t) * np.max(np.mean(S_T) - K, 0)
+nsteps = int(T / dt)  # Number of steps
+dW = np.sqrt(dt) * np.random.standard_normal(nsteps)
+S_euler = np.zeros(nsteps)
+S_euler[0] = S0
+
+for i in range(1, nsteps):
+    S_euler[i] = S_euler[i - 1] + r * S_euler[i - 1] * dt + v * S_euler[i - 1] * dW[i - 1]
+
+disc_payoff = np.exp(-r * T) * np.max(np.mean(S_euler) - K, 0)
+
+disc_payoff
+
+
+
+
+
+
+#building the simulation
+
+NSim = 10
+disc_payoff  = np.zeros(NSim)
+nsteps = int(T / dt)
+
+
+S_euler = np.zeros((NSim, nsteps))  # Number of steps
+dW = np.zeros((NSim, nsteps))  # Number of steps
+disc_payoff = np.zeros(NSim)
+
+
+for j in range(NSim):
+    dW[j] = np.sqrt(dt) * np.random.standard_normal(nsteps)
+    S_euler[j][0] = S0
+
+    for i in range(1, nsteps):
+        S_euler[j][i] = S_euler[j][i - 1] + r * S_euler[j][i - 1] * dt + v * S_euler[j][i - 1] * dW[j][i - 1]
+    disc_payoff[j] = np.exp(-r * T) * np.max((np.mean(S_euler[j]) - K, 0))
+
+sim_C = np.mean(disc_payoff)
+sim_C
+
+
+val = np.mean(S_euler[5]) - K
+val
+np.max((val , 0))
+K
+
+#what is this?!
+ST = S0 * np.exp((r - .5 * v**2) * T + v * np.cumsum(np.sqrt(T) * np.random.standard_normal(nsteps)))
+ST
